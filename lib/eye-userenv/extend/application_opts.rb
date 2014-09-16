dd1f471bdc0810315fb0876f087dc0c9308eca90
Roman Simecek <roman@good2go.ch>
2014-09-16 15:34:03 +0200

    Rewrite the code now it is done how it should.

--- lib/eye-userenv/extend/application_opts.rb
+++ lib/eye-userenv/extend/application_opts.rb
@@ -1,8 +1,18 @@
-Eye::Dsl::ApplicationOpts.class_eval do
+Eye::Dsl::ApplicationOpts
+class Eye::Dsl::ApplicationOpts
 
-  def use_user_env_from(user_indent, bash_envs='.bash_envs')
-    infos     = Etc.getpwnam user_indent
-    file_path = File.join(infos.dir, bash_envs)
+  create_options_methods([:user_env_from], Hash)
+
+end
+
+Eye::Dsl::PureOpts
+class Eye::Dsl::PureOpts
+
+  def set_user_env_from(opts={})
+    raise Eye::Dsl::Error, "Missing uid and/or env_file option" unless opts.has_key?(:uid) || opts.has_key?(:env_file)
+
+    infos     = Etc.getpwnam opts[:uid]
+    file_path = File.join(infos.dir, opts[:env_file])
 
     envs = IO.read(file_path).lines.inject({}) do |out, line|
       if result = /(?<key>\p{Word}*)=(?<value>.*)/.match(line)
