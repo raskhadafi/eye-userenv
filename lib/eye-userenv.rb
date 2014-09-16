81cee06b3e74a97a04294eb99860a0b8576b18b1
Roman Simecek <roman@good2go.ch>
2014-09-16 16:00:24 +0200

    Refactoring.

--- lib/eye-userenv.rb
+++ lib/eye-userenv.rb
@@ -1,9 +1,4 @@
 require 'eye'
-require 'eye-userenv/version'
-require 'eye-userenv/extend/application_opts'
 
-module Eye
-  module Userenv
-    # Your code goes here...
-  end
-end
+require_relative 'eye-userenv/extend_eye'
+require_relative 'eye-userenv/version'
